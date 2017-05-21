223a6f783436773afbfea846807d68bc24ab8ddb
Wei Ding <deanwade@126.com>
2017-05-21 16:17:44 +0800

    use session header strategy

--- src/main/java/hello/config/HttpSessionConfig.java
+++ src/main/java/hello/config/HttpSessionConfig.java
@@ -1,6 +1,7 @@
 package hello.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
@@ -8,13 +9,13 @@ import org.springframework.session.web.http.HttpSessionStrategy;
 @EnableRedisHttpSession
 public class HttpSessionConfig {
 
-//	@Bean
-//	public LettuceConnectionFactory connectionFactory() {
-//		return new LettuceConnectionFactory(); // <2>
-//	}
+	@Bean
+	public LettuceConnectionFactory connectionFactory() {
+		return new LettuceConnectionFactory();
+	}
 
-//	@Bean
-//	public HttpSessionStrategy httpSessionStrategy() {
-//		return new HeaderHttpSessionStrategy(); // <3>
-//	}
+	@Bean
+	public HttpSessionStrategy httpSessionStrategy() {
+		return new HeaderHttpSessionStrategy();
+	}
 }
--- src/main/java/hello/controller/SessionController.java
+++ src/main/java/hello/controller/SessionController.java
@@ -10,28 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import hello.model.Greeting;
-
 @RestController
 @RequestMapping("/session")
 public class SessionController {
 
-//	@GetMapping(value = "/login", produces = "application/json")
 	@GetMapping("/login")
-	public Greeting helloUser(Principal principal) {
-//		HashMap<String, String> result = new HashMap<String, String>();
-//		result.put("username", principal.getName());
-		return new Greeting(0L, principal.getName());
+	public String helloUser(Principal principal, HttpSession session) {
+		return "Session: " + session.getId() + " created for user: " + principal.getName();
 	}
 	
 	@GetMapping("/home")
-	public Greeting home(HttpSession session) {
-		return new Greeting(0L, session.getId());
+	public String home(HttpSession session) {
+		return session.getId() + " at /home";
 	}
 
 	@GetMapping("/logout")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void logout(HttpSession session) {
+	public String logout(HttpSession session) {
 		session.invalidate();
+		return session.getId() + " invalidated";
 	}
 }
